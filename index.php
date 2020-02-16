78d9dd5402a16c8d3cb6c96f56ec0ceb7e8fe917
Jhorge_klicks <acquayegeorge9@gmail.com>
2020-02-16 02:10:36 -0800

    code optimized

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>My Mulitiplication System</title>
-    <!--    <link rel="stylesheet" href="css/style.css">-->
+    <link rel="stylesheet" href="css/style.css">git
 </head>
 
 <body>
